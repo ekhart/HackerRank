dec6e34d66a32ccfffcadf7e5fe9e3670f454d5c
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-11 17:21:10 +0200

    read first line from test-case.txt

--- src/hackerrank/save_the_prisoner.clj
+++ src/hackerrank/save_the_prisoner.clj
@@ -1,13 +1,31 @@
 (ns hackerrank.save-the-prisoner)
+(require '[clojure.java.io :as io])
 
 ;; https://www.hackerrank.com/challenges/save-the-prisoner
 
+(defn parseInt [n]
+	(Integer/parseInt n))
+
+; http://stackoverflow.com/questions/7756909/in-clojure-1-3-how-to-read-and-write-a-file
 (defn get-current-path []
 	(System/getProperty "user.dir"))
 ; (println (get-current-path))
 
-(defn parseInt [n]
-	(Integer/parseInt n))
+(def test-file-path "resources\\test-case.txt")
+; (println (slurp test-file-path))
+
+; http://stackoverflow.com/questions/36579613/how-to-read-n-lines-from-a-file-in-clojure
+; process-file-by-lines: http://stackoverflow.com/questions/25948813/read-line-by-line-for-big-files
+(defn lines [n filename]
+	(with-open [rdr (io/reader filename)]
+		(doall (take n (line-seq rdr)))))
+
+(defn read-line []
+	(lines 1 test-file-path))
+
+(defn get-test-case-number []
+	(parseInt (read-line)))
+
 
 (defn test-read-line [file])
 
--- test/hackerrank/save_the_prisoner_test.clj
+++ test/hackerrank/save_the_prisoner_test.clj
@@ -6,5 +6,11 @@
   (testing "parseInt"
     (is (= (parseInt "1") 1))))
 
+(deftest read-line-test
+	(is (= (read-line) "1")))
+
 (deftest get-current-path-test
 	(is (= (get-current-path) "H:\\github\\hackerrank")))
+
+(deftest get-test-case-number-test
+	(is (= (get-test-case-number) 1)))
