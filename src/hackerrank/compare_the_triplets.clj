0bdd155f570d96f0649820a2bb2c0c3cc8434a4f
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-28 10:08:36 +0200

    fix requires

--- src/hackerrank/compare_the_triplets.clj
+++ src/hackerrank/compare_the_triplets.clj
@@ -1,5 +1,5 @@
 (ns hackerrank.compare-the-triplets
 	(:require [clojure.java.io :as io]
-		[hackerrank.save-the-prisoner :refer :all]))
+	 	[hackerrank.save-the-prisoner :refer :all]))
 
 ; https://www.hackerrank.com/challenges/compare-the-triplets
