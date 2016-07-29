7a2f1de2073b8074f1ca954520eacd3573b3f7a7
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-29 16:57:52 +0200

    compare the triplets working solution

--- src/hackerrank/compare_the_triplets.clj
+++ src/hackerrank/compare_the_triplets.clj
@@ -6,6 +6,15 @@
 ; https://www.hackerrank.com/challenges/compare-the-triplets
 
 
+(def alice (atom 0))
+(def bob (atom 0))
+
+;; https://clojuredocs.org/clojure.core/swap!
+;; https://clojuredocs.org/clojure.core/cond
+(defn score [a b]
+  (cond (= (compare a b) 1) (swap! alice inc)
+        (= (compare a b) -1) (swap! bob inc)))
+
 (let [a0_temp "5 6 7" ; (read-line)
       a0_t (split a0_temp #"\s+")
       a0 (parseInt (a0_t 0))
@@ -21,4 +30,10 @@
   ;; https://clojuredocs.org/clojure.string/join
   (println (join " " [a0 a1 a2]))
   (println (join " " [b0 b1 b2]))
+
+  (score a0 b0)
+  (score a1 b1)
+  (score a2 b2)
+
+  (println @alice " " @bob)
 )
