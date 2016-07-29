b4ece9f41b145e052b61cc8b050df54091edcb30
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-29 17:04:03 +0200

    better output, use Integer/parseInt, remember about max points

--- src/hackerrank/compare_the_triplets.clj
+++ src/hackerrank/compare_the_triplets.clj
@@ -6,26 +6,27 @@
 ; https://www.hackerrank.com/challenges/compare-the-triplets
 
 
+(def max-points 100)
 (def alice (atom 0))
 (def bob (atom 0))
 
 ;; https://clojuredocs.org/clojure.core/swap!
 ;; https://clojuredocs.org/clojure.core/cond
 (defn score [a b]
-  (cond (= (compare a b) 1) (swap! alice inc)
-        (= (compare a b) -1) (swap! bob inc)))
+  (cond (and (> (compare a b) 0) (< @alice max-points)) (swap! alice inc)
+        (and (< (compare a b) 0) (< @bob max-points)) (swap! bob inc)))
 
 (let [a0_temp "5 6 7" ; (read-line)
       a0_t (split a0_temp #"\s+")
-      a0 (parseInt (a0_t 0))
-      a1 (parseInt (a0_t 1))
-      a2 (parseInt (a0_t 2))
+      a0 (Integer/parseInt (a0_t 0))
+      a1 (Integer/parseInt (a0_t 1))
+      a2 (Integer/parseInt (a0_t 2))
 
       b0_temp "3 6 10" ;(read-line)
       b0_t (split b0_temp #"\s+")
-      b0 (parseInt (b0_t 0))
-      b1 (parseInt (b0_t 1))
-      b2 (parseInt (b0_t 2))]
+      b0 (Integer/parseInt (b0_t 0))
+      b1 (Integer/parseInt (b0_t 1))
+      b2 (Integer/parseInt (b0_t 2))]
 
   ;; https://clojuredocs.org/clojure.string/join
   (println (join " " [a0 a1 a2]))
@@ -35,5 +36,5 @@
   (score a1 b1)
   (score a2 b2)
 
-  (println @alice " " @bob)
+  (println @alice @bob)
 )
