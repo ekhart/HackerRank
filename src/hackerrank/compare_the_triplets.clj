8dc1a56d0ab3f2b15151f6fac86564823b3c3480
Artur Dębkowski <arturdebkowski@gmail.com>
2016-07-29 16:45:19 +0200

    compare tests and parseInt

--- src/hackerrank/compare_the_triplets.clj
+++ src/hackerrank/compare_the_triplets.clj
@@ -1,19 +1,24 @@
 (ns hackerrank.compare-the-triplets
 	(:require [clojure.java.io :as io]
             [hackerrank.save-the-prisoner :refer :all])
-  (:use [clojure.string :only (split)]))
+  (:use [clojure.string :only (split join)]))
 
 ; https://www.hackerrank.com/challenges/compare-the-triplets
 
+
 (let [a0_temp "5 6 7" ; (read-line)
       a0_t (split a0_temp #"\s+")
-      a0 (Integer/parseInt (a0_t 0))
-      a1 (Integer/parseInt (a0_t 1))
-      a2 (Integer/parseInt (a0_t 2))
+      a0 (parseInt (a0_t 0))
+      a1 (parseInt (a0_t 1))
+      a2 (parseInt (a0_t 2))
 
       b0_temp "3 6 10" ;(read-line)
       b0_t (split b0_temp #"\s+")
-      b0 (Integer/parseInt (b0_t 0))
-      b1 (Integer/parseInt (b0_t 1))
-      b2 (Integer/parseInt (b0_t 2))]
+      b0 (parseInt (b0_t 0))
+      b1 (parseInt (b0_t 1))
+      b2 (parseInt (b0_t 2))]
+
+  ;; https://clojuredocs.org/clojure.string/join
+  (println (join " " [a0 a1 a2]))
+  (println (join " " [b0 b1 b2]))
 )
--- test/hackerrank/compare_the_triplets_test.clj
+++ test/hackerrank/compare_the_triplets_test.clj
@@ -3,9 +3,12 @@
             [hackerrank.save-the-prisoner :refer :all]
             [hackerrank.compare-the-triplets :refer :all]))
 
+; run test
+; lein test hackerrank.compare-the-triplets-test
+
 (deftest parseInt-test
   (testing "parseInt"
-    (is (= (parseInt "0") 1))))
+    (is (= (parseInt "1") 1))))
 
 ; (deftest read-line-test
 ; 	(is (= (read-line) "1")))
@@ -15,3 +18,9 @@
 
 ; (deftest get-test-case-number-test
 ; 	(is (= (get-test-case-number) 1)))
+
+(deftest compare-test
+  (testing "compare"
+    (is (= (compare 5 3) 1))
+    (is (= (compare 6 6) 0))
+    (is (= (compare 7 10) -1))))
