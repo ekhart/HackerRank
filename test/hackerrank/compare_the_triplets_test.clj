c62e3997ddfaf7308325cc9746f56375fa62419b
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-02 15:06:25 +0200

    base test case

--- test/hackerrank/compare_the_triplets_test.clj
+++ test/hackerrank/compare_the_triplets_test.clj
@@ -1,26 +1,21 @@
 (ns hackerrank.compare-the-triplets-test
   (:require [clojure.test :refer :all]
-            [hackerrank.save-the-prisoner :refer :all]
+;;             [hackerrank.save-the-prisoner :refer :all]
             [hackerrank.compare-the-triplets :refer :all]))
 
 ; run test
 ; lein test hackerrank.compare-the-triplets-test
+; lein.bat test hackerrank.compare-the-triplets-test
 
 (deftest parseInt-test
   (testing "parseInt"
     (is (= (parseInt "1") 1))))
 
-; (deftest read-line-test
-; 	(is (= (read-line) "1")))
-
-; (deftest get-current-path-test
-; 	(is (= (get-current-path) "H:\\github\\hackerrank")))
-
-; (deftest get-test-case-number-test
-; 	(is (= (get-test-case-number) 1)))
-
 (deftest compare-test
   (testing "compare"
     (is (= (compare 5 3) 1))
     (is (= (compare 6 6) 0))
-    (is (= (compare 7 10) -1))))
+    (is (= (compare 7 10) -1)))
+
+  (testing "compare-triplets"
+    (is (compare-triplets "5 6 7" "3 6 10") "1 1")))
