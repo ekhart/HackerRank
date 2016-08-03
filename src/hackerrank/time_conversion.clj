c64ad35e16e5e8e07f2ee2de9da3e5bc0706bc1f
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-03 13:54:30 +0200

    cleaned solution, todo

--- src/hackerrank/time_conversion.clj
+++ src/hackerrank/time_conversion.clj
@@ -27,22 +27,19 @@
 (defn remove-am-pm [time]
   (subs time 0 (- (count time) 2)))
 
-; http://clojure.org/reference/sequences
-; https://clojuredocs.org/clojure.string/join
 (defn pm-time [time]
   (let [hours (+ (hour time) 12)
         minutes-and-seconds (rest (time-parts time))]
-    (clojure.string/join ":" (cons hours minutes-and-seconds))))
+    (join ":" (cons hours minutes-and-seconds))))
 
 (defn hour-time [hours time]
-   (clojure.string/join ":" (cons hours (rest (time-parts time)))))
+   (join ":" (cons hours (rest (time-parts time)))))
 
 (defn format-24h [time]
   (if (and (pm? time) (not (= (hour time) 12)))
     (remove-am-pm (pm-time time))
     (remove-am-pm time)))
 
-; https://clojuredocs.org/clojure.core/cond
 (defn pm-am-time [time]
   (let [hours (hour time)
         time24 (cond
@@ -54,4 +51,9 @@
 (defn time-conversion [line]
   (pm-am-time line))
 
-(println (time-conversion "07:05:45PM"))
+;; (println (time-conversion "07:05:45PM"))
+;; for hackerrank
+;; (println (time-conversion (read-line)))
+
+;; 2016-08-03 13:50 run at hackerrank: 7/10 solution - 3 fails
+;; o preper more test cases
