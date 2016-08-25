21c0f1b3c4a8d8f1f2b66e97b552a76afed683f5
Artur Dębkowski <arturdebkowski@gmail.com>
2016-08-25 15:41:48 +0200

    Evaluating e^x resolved

--- src/hackerrank/fp/Evaluating e^x.clj
+++ src/hackerrank/fp/Evaluating e^x.clj
@@ -5,5 +5,19 @@
 (defn factorial [n]
 	(reduce * (range 1 (inc n))))
 
-(println (exp 2 3))
-(println (factorial 4))
+(defn ex [x]
+  (float (reduce #(+ %1 (/ (exp x %2) (factorial %2))) 0 (range 10))))
+
+;; (println (exp 2 3))
+;; (println (factorial 4))
+;; (println (ex 0))
+;; (println (ex 20))
+
+(exp 2 3)
+(factorial 4)
+
+(ex 0)
+(ex 20)
+(ex 5)
+(ex 0.5)
+(ex -0.5)
