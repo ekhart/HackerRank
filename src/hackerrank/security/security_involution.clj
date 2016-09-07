29e4a355b58c2f5bca9767481483eadc21fa204b
Artur Dębkowski <arturdebkowski@gmail.com>
2016-09-07 13:48:12 +0200

    added Security Involution task

---
+++ src/hackerrank/security/security_involution.clj
@@ -0,0 +1,19 @@
+;; Security Involution
+;; https://www.hackerrank.com/challenges/security-involution/forum
+
+(ns hackerrank.security.security-involution
+  (:require [clojure.java.io :as io])
+  (:use [clojure.string :only (split join trim)]))
+
+(def input "2\n2 1")
+
+(defn parseInt [string]
+  (Integer/parseInt string))
+
+(with-in-str input
+  (let [n (parseInt (read-line))
+        numbers (concat [0](map parseInt (split (read-line) #" ")))]
+
+      (doseq [i (range 1 (inc n))]
+;;         (println i (nth numbers i))
+        (println (nth numbers (nth numbers i))))))
