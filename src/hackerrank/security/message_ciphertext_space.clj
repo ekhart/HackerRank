20e65ee58a29864922c49a2415da5e19d297ef40
Artur Dębkowski <arturdebkowski@gmail.com>
2016-09-08 12:40:46 +0200

    working solution

--- src/hackerrank/security/message_ciphertext_space.clj
+++ src/hackerrank/security/message_ciphertext_space.clj
@@ -8,10 +8,39 @@
 (defn parseInt [string]
   (Integer/parseInt string))
 
+;; solution idea:
+;; map "[0-9]" ot "[1-90]
+;; ("0", "1", ..., "9") -> ("1", "2", ..., "0")
+;; ->
+(def before (seq "0123456789"))
+(def after (seq "1234567890"))
+;; zip colls to dict (key value)
+;; (map #(println %1 %2) before after)
+
+;; http://stackoverflow.com/questions/2588227/is-there-an-equivalent-for-the-zip-function-in-clojure-core-or-contrib
+;; http://stackoverflow.com/questions/5088803/in-clojure-is-there-an-easy-way-to-convert-between-list-types
+(def zipped (into {} (map hash-map before after)))
+
+;; (nth zipped 1)
+
+;; (into {} zipped)
+zipped
+
+;; each element of input string map using zipped to coresponding char
+(map zipped (seq "982"))
+
+;; joins chars - get string
+(apply str (map zipped (seq "982")))
+
 (defn message-ciphertext-space
+
   ([]
-    (let [n (parseInt (read-line))]
-        n))
+    (def before (seq "0123456789"))
+    (def after (seq "1234567890"))
+    (def zipped (into {} (map hash-map before after)))
+
+   (let [n (trim (read-line))]
+        (apply str (map zipped (seq "982")))))
 
   ([input]
    (with-in-str input
--- test/hackerrank/security/message_ciphertext_space_test.clj
+++ test/hackerrank/security/message_ciphertext_space_test.clj
@@ -8,12 +8,17 @@
 
 (def input "982")
 
+(seq "0123456789")
+(seq "9012345678")
+
 (deftest message-ciphertext-space-test
 
   (testing "parseInt"
     (is (= (parseInt "1") 1)))
 
+
+
   (testing "message-ciphertext-space"
-    (is (= (message-ciphertext-space input) 982)))
+    (is (= (message-ciphertext-space input) "093")))
 )
 
