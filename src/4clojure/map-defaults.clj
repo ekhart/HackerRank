ec09854e91fae7eaa8a461b362371ae1841e8494
Artur Dębkowski <arturdebkowski@gmail.com>
2016-10-11 19:07:45 +0200

    solved 4Clojure: Map Default

---
+++ src/4clojure/map-defaults.clj
@@ -0,0 +1,14 @@
+(defn map-default [default coll]
+  (assoc {} (first coll) default)
+  (loop [l {} r coll]
+    (if (empty? r)
+      l
+      (recur
+        (assoc l (first r) default)
+        (rest r)))))
+
+(= 2 (:foo {:bar 0, :baz 1} 2))
+
+(= (map-default 0 [:a :b :c]) {:a 0 :b 0 :c 0})
+(= (map-default "x" [1 2 3]) {1 "x" 2 "x" 3 "x"})
+(= (map-default [:a :b] [:foo :bar]) {:foo [:a :b] :bar [:a :b]})
