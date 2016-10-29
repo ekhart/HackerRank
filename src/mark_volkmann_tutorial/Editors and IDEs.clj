1cad76fc155f8062f9a7443e245b8a7b2fd64a87
Artur Dębkowski <arturdebkowski@gmail.com>
2016-10-29 19:55:48 +0200

    IDEs

---
+++ src/mark_volkmann_tutorial/Editors and IDEs.clj
@@ -0,0 +1,13 @@
+;; Editors and IDEs
+;; http://java.ociweb.com/mark/clojure/article.html#EditorsIDEs
+
+;; emacs
+;; slime
+;; vim
+;; NetBeans
+;; IDEA
+;; Eclipse
+
+;; ;; my
+;; ; VS clojure
+;; ; https://www.google.pl/search?q=visual+studio+clojure&gws_rd=cr&ei=MeEUWODqKurt6ASNyrmYAw
