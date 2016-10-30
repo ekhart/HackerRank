7eecb1d23399250262421eb04390c6cb598477e2
Artur Dębkowski <arturdebkowski@gmail.com>
2016-10-30 17:02:03 +0100

    conclusions

---
+++ src/mark_volkmann_tutorial/conclusion.clj
@@ -0,0 +1,14 @@
+;; Conclusion
+;; http://java.ociweb.com/mark/clojure/article.html#Conclusion
+
+;; more
+;; Programming Clojure by Stuart Halloway.
+
+;; easier concurrent programming
+;; functional programming vs OOP
+;; run on JVM
+;; dynamic vs static typed
+;; Lisp syntax
+
+;; References
+;; http://java.ociweb.com/mark/clojure/article.html#References
