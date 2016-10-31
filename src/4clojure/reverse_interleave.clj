b196c38d629477a85216221c201a3d7c28e468c6
Artur Dębkowski <arturdebkowski@gmail.com>
2016-10-31 23:35:18 +0100

    reverse interleave continure

--- src/4clojure/reverse_interleave.clj
+++ src/4clojure/reverse_interleave.clj
@@ -7,11 +7,21 @@
 
 
 (defn __ [coll n]
-;;   (partition-by #(mod % (inc n)) coll))
+  (partition-by #(mod % (inc n)) coll))
 
 ;; (mod 5 3)
 
-(map-indexed #(mod %1
+;; (nth [] 0)
+
+;; (map-indexed (fn [i a] (mod i 2)) [1 2 3 4 5 6])
+
+;; (reduce-kv
+
+;; (repeat 2 [])
+
+(conj (nth %1
+
+(reduce #(conj (nth %1 (mod ) (repeat 2 []) [1 2 3 4 5 6])
 
 (= (__ [1 2 3 4 5 6] 2) '((1 3 5) (2 4 6)))
 
