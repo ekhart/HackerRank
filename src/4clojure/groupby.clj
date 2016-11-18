991a654f660fa1fc78ea5f131c882c07ed998fcb
Artur Dębkowski <arturdebkowski@gmail.com>
2016-11-18 15:40:51 +0100

    group by WIP

---
+++ src/4clojure/groupby.clj
@@ -0,0 +1,31 @@
+;; Group a Sequence
+;; http://www.4clojure.com/problem/63
+
+;; Difficulty:	Easy
+;; Topics:	core-functions
+
+
+;; Given a function f and a sequence s, write a function which returns a map. The keys should be the values of f applied to each item in s. The value at each key should be a vector of corresponding items in the order they appear in s.
+
+
+(defn __ [f coll]
+  (reduce #(assoc-in %1 [(f %2)]
+                     (if-let [l (%1 %2)]
+                       [%2]
+                       (conj l %2)))
+          {} coll))
+
+;; (reduce conj [] [1 3 6 8])
+
+(assoc-in {} [1] [2])
+(assoc-in {1 [1]} [1] [2])
+(assoc-in {1 [1]} [2] [2])
+
+
+(= (__ #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})
+
+(= (__ #(apply / %) [[1 2] [2 4] [4 6] [3 6]])
+   {1/2 [[1 2] [2 4] [3 6]], 2/3 [[4 6]]})
+
+(= (__ count [[1] [1 2] [3] [1 2 3] [2 3]])
+   {1 [[1] [3]], 2 [[1 2] [2 3]], 3 [[1 2 3]]})
