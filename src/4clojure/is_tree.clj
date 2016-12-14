7af700d093fbdaf959409577d8518d602e8d7058
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-14 11:18:54 +0100

    solved

--- src/4clojure/is_tree.clj
+++ src/4clojure/is_tree.clj
@@ -20,7 +20,7 @@
     (and (not (or (= coll true) (= coll false)))
          (= (count coll) 3)
          (node-or-nil? (second coll))
-         (node-or-nil? (nil? _3)))))
+         (node-or-nil? (nth coll 2)))))
 
 (node? '(:a (:b nil nil) nil))
 (node? '(:a (:b nil nil)))
@@ -50,3 +50,52 @@
 
 (= (__ '(:a nil ()))
    false)
+
+
+;; 4Clojure copy paste
+(fn node? [coll]
+  (letfn [(node-or-nil? [a] (or (node? a) (nil? a)))]
+    (and (not (or (= coll true) (= coll false)))
+         (= (count coll) 3)
+         (node-or-nil? (second coll))
+         (node-or-nil? (nth coll 2)))))
+
+
+;; _artem_uv's solution:
+(fn tree? [s]
+  (cond
+   (or (seq? s) (vector? s))
+   (and (= (count s) 3) (tree? (second s)) (tree? (last s)))
+   (nil? s) true
+   :else false))
+
+;; _pcl's solution:
+(fn tree? [t]
+  (if (= 3 (count t))
+    (every? #(or (nil? %) (and (sequential? %) (tree? %))) (rest t))
+    false))
+
+;; aceeca1's solution:
+(fn is-tree [x] (or (nil? x) (and
+    (sequential? x)
+    (= 3 (count x))
+    (is-tree (nth x 1))
+    (is-tree (nth x 2)))))
+
+;; adereth's solution:
+(fn [root] (every? #(or (nil? %) (and (sequential? %) (= 3 (count %))))
+                   (tree-seq #(and (sequential? %)
+                                   (= (count %) 3))
+                             rest root)))
+
+;; 0x89's solution:
+(fn tree? [x]
+   (or
+     (nil? x)
+     (and
+       (sequential? x)
+       (= 3 (count x))
+       (not (sequential? (first x)))
+       (tree? (second x))
+       (tree? (nth x 2))
+       )))
