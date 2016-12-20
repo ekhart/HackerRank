9977e681abb1d23aef1c4190174076e5abc69fc4
Artur Dębkowski <arturdebkowski@gmail.com>
2016-12-20 18:09:03 +0100

    done

--- src/4clojure/infix_calc.clj
+++ src/4clojure/infix_calc.clj
@@ -9,7 +9,33 @@
 
 
 (defn __ [& args]
- args)
+;;  ((nth args 1) (first args) (nth args 2)))
+  (reduce #(if (fn? (second %2))
+             ((second %2) %1 (nth args (inc (first %2))))
+             %1)
+          (first args)
+          (map-indexed vector args)))
+;;   (map-indexed vector args))
+
+(fn? 1)
+
+(boolean (some #{+} [- 1 3]))
+
+
+(next [1])
+
+(__ 2 + 5)
+
+
+
+(defn __ [& args]
+  (->> args
+      (map-indexed vector)
+      (reduce #(if (fn? (second %2))
+             ((second %2) %1 (nth args (inc (first %2))))
+             %1)
+          (first args))))
+
 
 
 (= 7  (__ 2 + 5))
@@ -19,3 +45,54 @@
 (= 8  (__ 10 / 2 - 1 * 2))
 
 (= 72 (__ 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9))
+
+
+(defn __ [& args]
+  (reduce #(if (fn? (second %2))
+             ((second %2) %1 (nth args (inc (first %2))))
+             %1)
+          (first args)
+          (map-indexed vector args)))
+
+;; (->> [2 + 5] (map-indexed vector))
+
+
+;; 4clojure copy paste
+(fn [& args]
+  (->> args
+      (map-indexed vector)
+      (reduce #(if (fn? (second %2))
+             ((second %2) %1 (nth args (incfirst %2))))
+             %1)
+          (first args)))
+
+
+
+;; _artem_uv's solution:
+(fn
+  [a1 & args]
+  (if (empty? args)
+    a1
+  (recur ((first args) a1 (second args)) (drop 2 args))))
+
+;; _pcl's solution:
+(fn [& xs]
+  (reduce #((first %2) %1 (last %2)) (first xs) (partition 2 (rest xs))))
+
+;; aceeca1's solution:
+(fn infix [init & rawops] (let [
+    ops (partition 2 rawops)
+    do-op (fn [v op] ((first op) v (second op)))]
+    (reduce do-op init ops)))
+
+;; adereth's solution:
+(fn [firstlhs & t]
+  (let [oprhs (partition 2 t)]
+    (reduce (fn [lhs [op rhs]] (op lhs rhs)) firstlhs oprhs)))
+
+;; 0x89's solution:
+(fn infix
+  ([x op y] (op x y))
+  ([x op y & xs]
+   (apply infix (cons (infix x op y)
+                xs))))
